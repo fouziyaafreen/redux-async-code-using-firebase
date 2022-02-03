4141b5acaf46b02e486db125fd70f30ff837fb99
Fouziya Afreen <42962887+fouziyaafreen@users.noreply.github.com>
2022-02-03 19:33:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Cart from "./components/Cart/Cart";
+import { useEffect, Fragment } from "react";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { sendCartData,fetchCarthData } from "./components/store/cart-actions";
+
+let initState=true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCarthData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (initState) {
+      initState = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+        status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
